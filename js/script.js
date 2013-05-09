72b47c51fa522d6c00c88b1b1aec3c26dc074197
Esther Brunner <wikidesign@gmail.com>
2013-05-09 20:22:57 +0200

    Added components for search and paging

--- js/script.js
+++ js/script.js
@@ -16,3 +16,21 @@ $('#page-overlay').on('click', function(e){
 $('#wrapper').on('click', function(){
   $('body').toggleClass('navbars-hidden');
 });
+
+// Fixed header
+var headerHeight = $('#header').height();
+$('#header + .spacer').css('height', headerHeight);
+$('#header').affix({
+  offset: {
+    top: headerHeight - $('#header .navbar').height()
+  }
+});
+
+// Fixed footer
+var footerHeight = $('#header').height();
+$('#footer + .spacer').css('height', footerHeight);
+$('#footer').affix({
+  offset: {
+    bottom: footerHeight - $('#footer .navbar').height()
+  }
+}).addClass('affix');
