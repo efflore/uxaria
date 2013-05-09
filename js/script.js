cac820476bfdda5ba3cacdc3891dd4e2025172d4
Esther Brunner <wikidesign@gmail.com>
2013-05-09 15:27:20 +0200

    Implemented navigation sidebar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+// Toggle navigation
+$('a.sidebar-toggle').on('click', function(e){
+  e.preventDefault();
+  e.stopPropagation();
+  var target = $(this).attr('aria-controls');
+  $('body').toggleClass(target + '-open');
+});
 
+// Remove overlay
+$('#page-overlay').on('click', function(e){
+  e.stopPropagation();
+  $('body').removeAttr('class');
+});
+
+// Toggle fixed navbars
+$('#wrapper').on('click', function(){
+  $('body').toggleClass('navbars-hidden');
+});
