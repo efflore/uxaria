2a8d0f52b985ce92bddeaf95d0a54db42c2c48af
Esther Brunner <wikidesign@gmail.com>
2013-05-10 02:27:51 +0200

    Multi-level navigation with animation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Toggle navigation
+// Toggle sidebars
 $('a.sidebar-toggle').on('click', function(e){
   e.preventDefault();
   e.stopPropagation();
@@ -34,3 +34,23 @@ $('#footer').affix({
     bottom: footerHeight - $('#footer .navbar').height()
   }
 }).addClass('affix');
+
+// Navigation: go up
+$('#nav-back a').on('click', function(e){
+  e.preventDefault();
+  e.stopPropagation();
+  $('#nav .nav-list').removeClass('depth1');
+  $(this).text('Uxaria Homepage');
+});
+
+// Navigation: show subitems
+$('.nav a.children').on('click', function(e){
+  e.preventDefault();
+  e.stopPropagation();
+  $(this).parent().siblings().removeClass('selected')       // deselect previous selected item
+    .children('ul.subitems-menu').addClass('hide');         // hide subitems of sibling menuitems
+  $(this).parent().addClass('selected');                    // select current item
+  $(this).siblings('ul.subitems-menu').removeClass('hide'); // show subitems of selected item
+  $(this).closest('.nav-list').addClass('depth1');
+  $('#nav-back a').text($(this).text());
+});
