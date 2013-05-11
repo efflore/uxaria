4a6a82d5380498703aae3b1d7e4116518b214296
Esther Brunner <wikidesign@gmail.com>
2013-05-11 13:40:41 +0200

    Set up the application

--- README.md
+++ README.md
@@ -1,4 +1,62 @@
-uxaria
+Uxaria
 ======
 
-Uxaria Web Interface Toolkit
+Uxaria is a User Interface Toolkit for the Web.
+
+It's built with some of the best open source libraries for web development:
+
+  * Silex PHP micro-framework
+  * Twig templating engine
+  * jQuery javascript library
+  * Modernizr javascript feature detection
+  * Twitter Bootstrap for HTML, CSS and Javascript components
+  
+Setup
+-----
+
+1. Clone this repository to a folder on a web server.
+
+2. Ensure the following PHP setting in php.ini:
+
+    ```
+    detect_unicode = Off
+    date.timezone = UTC
+    ```
+    
+3. You Apache configuration should include something like this:
+
+    ```
+    <Directory "/Users/~**user**/Sites/">
+      Options Indexes MultiViews FollowSymLinks
+      AllowOverride All
+      Order allow,deny
+      Allow from all
+    </Directory>
+    ```
+    
+4. Open a terminal window for your project directory and enter these commands:
+
+    ```sh
+    $ curl -s http://getcomposer.org/installer | php
+    $ php composer.phar install
+    ```
+
+5. Setup .htaccess in `/web/` directory:
+
+    ```
+    <IfModule mod_rewrite.c>
+       RewriteEngine On
+       RewriteBase /~**user**/web-toolkit/web
+       RewriteCond %{REQUEST_FILENAME} !-f
+       RewriteCond %{REQUEST_FILENAME} !-d
+       RewriteRule ^(.*)$ index.php [QSA,L]
+    </IfModule>
+    ```
+
+Update
+------
+
+    ```sh
+    $ php composer.phar self-update
+    $ php composer.phar update
+    ```
---
+++ web/index.php
@@ -0,0 +1,89 @@
+<?php
+
+// Define Constants
+
+if (!defined('APP_DIR')){
+  define('APP_DIR', __DIR__.'/../app');
+}
+
+if (!defined('VENDOR_DIR')){
+  define('VENDOR_DIR', __DIR__.'/../vendor');
+}
+
+if (!defined('DATA_DIR')){
+  define('DATA_DIR', __DIR__.'/../data');
+}
+
+if (!defined('CACHE_DIR')){
+  define('CACHE_DIR', __DIR__.'/../cache');
+}
+
+// Load application
+
+require_once VENDOR_DIR.'/autoload.php';
+
+$app = new Silex\Application();
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+  'twig.path' => DATA_DIR,
+  'twig.options' => array(
+    'cache' => $app['debug'] ? false : CACHE_DIR.'/twig',
+    'debug' => $app['debug']
+  )
+));
+$app['twig']->addExtension(new Twig_Extension_Debug());
+
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+// Define routes
+
+// Generate CSS
+$app->get('/styles/{id}.css', function($id) use ($app){
+  $input =  realpath(DATA_DIR.'/styles/'.$id.'.less');
+  $output = realpath(__DIR__.'/css/'.$id.'.css');
+  
+  if (!@file_exists($input)){
+    $content = '';
+  } else {
+    require_once VENDOR_DIR.'/leafo/lessphp/lessc.inc.php';
+    $less = new lessc;
+    $content = $less->compileFile($input);  // development
+    $less->setFormatter('compressed');
+    $less->checkedCompile($input, $output); // production
+  }
+  $response = new Response($content);
+  $response->headers->set('Content-Type', 'text/css');
+  return $response;
+})
+->bind('css');
+
+// Pages
+$app->get('/{id}', function($id) use ($app){
+  $path = '/pages/'.$id.'/index.html';
+  if (!@file_exists(DATA_DIR.$path)){
+    $app->abort(404, 'Sorry, this page does not exist.');
+  }
+  return $app['twig']->render($path, array('id' => $id));
+})
+->value('id', 'start')
+->bind('page');
+
+// Pages in folders
+$app->get('/{folder}/{id}', function($folder, $id) use ($app){
+  $path = '/pages/'.$folder.'/'.$id.'/index.html';
+  if (!@file_exists(DATA_DIR.$path)){
+    $app->abort(404, 'Sorry, this page does not exist.');
+  }
+  return $app['twig']->render($path, array('folder' => $folder, 'id' => $id));
+})
+->value('id', 'start')
+->bind('subpage');
+
+$app->run();
+
+?>
